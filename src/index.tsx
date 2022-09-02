12ff5fd78b084149a34746eb630388c392508bc4
Fajar Abdi Nugraha <nugrahafajar37@gmail.com>
2022-09-02 16:39:30 +0700

    change default theme to dark

--- src/index.tsx
+++ src/index.tsx
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(document.getElementById('root')!);
 root.render(
   <React.StrictMode>
     <ChakraProvider theme={CustomeTheme}>
-    <ColorModeScript initialColorMode='light'></ColorModeScript>
+    <ColorModeScript initialColorMode='dark'></ColorModeScript>
       <BrowserRouter>
         <App>
         </App>
